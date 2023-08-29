32504aebff0c2dfacc5c6d58a0481c842dc4c199
Ranu Putra Dewantara <ranuputradewantara@vk.com>
2023-08-29 17:59:27 +0700

    1st upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'font1': 'Oxanium',
+        'font2': 'Tektur',
+        'font3': 'Coda',
+        'font4': 'DAGGERSQUARE',
+        'font5': 'Goldman',
+        'font6': 'Prosto One',
+        'font7': 'Play',
+        'font8': 'Revalia',
+        'font9': 'Iceberg',
+        'font10': 'Good Times',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
+
