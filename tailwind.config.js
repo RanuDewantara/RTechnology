bf0a3a6d7daa3268fbb77864131149b8e3dc42db
RanuDewantara <ranuputradewantara@vk.com>
2024-04-06 23:51:22 +0700

    Fix: Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        'font1': 'Oxanium',
-        'font2': 'Tektur',
-        'font3': 'Coda',
-        'font4': 'DAGGERSQUARE',
-        'font5': 'Goldman',
-        'font6': 'Play',
-      }
     },
   },
   plugins: [
